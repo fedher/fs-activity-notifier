3d1ec76fce6a24dc55d823bcac29351588ab5bdd
J. Federico Hernandez <fede.hernandez@gmail.com>
2012-08-21 00:08:32 -0300

    Subject text changed.

--- mail.h
+++ mail.h
@@ -2,7 +2,7 @@
 #define _MAIL_H_
 
 #define MAIL_CMD "/usr/sbin/sendmail -t %s"
-#define MAIL_SUBJECT "samba notification"
+#define MAIL_SUBJECT "fs activity notification"
 #define MAIL_TO "user@test.com"
 #define MAIL_FROM "topo@test.com"
 
